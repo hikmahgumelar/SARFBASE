2165860b6c7f40319e4d925c776921e0a38caf56
hikmahgumelar <hikmahgumelar@gmail.com>
2016-11-26 13:35:12 +0700

    branch addsocket last commit success

--- server/server.js
+++ server/server.js
@@ -1,18 +1,18 @@
 var express = require('express');
 var app = express();
-var port = process.env.PORT || 8888;
+var port = process.env.PORT || 1234;
 var io = require('socket.io')(12345)
 
 // start the server
 app.listen(port);
 console.log('Server jalan.....');
 console.log('menungu masukan....');
+io.on('connection', function(socket){
+console.log('ada yang konek');
 
 // routes will go here
 app.get('/', function(req, res) {
 var nama_sensor =  req.param('s');
-io.on('connection', function(socket){
-console.log('ada yang konek');
-io.emit('alert', 'dariserver');  
+io.emit('alert', nama_sensor);  
 });
 });
