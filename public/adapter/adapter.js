35682e48766c2679fd5d151914509f6cdcdef426
hikmahgumelar <hikmahgumelar@gmail.com>
2017-07-17 15:22:54 +0700

    add fitur  table

--- public/adapter/adapter.js
+++ public/adapter/adapter.js
@@ -1,7 +1,7 @@
 var target = 'http://api-push.ibstower.com'
 var port = '12345'
 var socket = io(target+':'+port)
-var pesan = "data dari client"	
+var pesan = "data dari client"
 //socket.on('alert', function(data){
  // console.log(data)
 //document.getElementById("test").innerHTML = data;
@@ -10,7 +10,6 @@ socket.on('alert', function(data){
 
 var a = Date();
 
-    $('#messages1').prepend($('<p>').text(a));
-    $('#messages2').prepend($('<p>').text(data));
+    $('#messages1').text(a);
+    $('#messages2').text(data);
  });
-
