09f357383efcd9158d1657103500a2f1d30dc9dc
hikmahgumelar <hikmahgumelar@gmail.com>
2017-07-31 12:51:21 +0700

    add module tambah device Iot

--- routes/index.js
+++ routes/index.js
@@ -39,13 +39,13 @@ router.get('/mon', function(req, res, next) {
 
 router.post('/tambahIoT', function(req, res ,next){
 var iotBaru = new iot({
-    id: req.body.namaid,
-    tanggal: "-",
-    site : req.body.namasite,
+    id: "0003",
+    tanggal: Date.now(),
+    site: "JAKARTA",
     temp: "0",
     hum: "0",
-    door: "-",
-    acpwr: "-"
+    door: "0",
+    acpwr: "0"
     });
     
  iotBaru.save(function(err){
@@ -63,34 +63,8 @@ router.get('/tambah', function(req, res){
 
   res.render('tambahIoT', {title : 'tambah perangkat IoT' })
 });
-    /* Halaman Perintah */
-    /*
-    router.get('/perintah', function(req, res){
-    	jsonfile.readFile(file, function(err, obj){
-    if (obj.light=="on"){
-    
-    warna = "red";
-    console.log	(warna);
-    return;
-    }
-    warna = "black";
-    console.log(warna);
-    	res.render('perintah', { title:"Perintah Pengendali" ,status: warna });
-    
-    });"
-});
->>>>>>> 3f04919889c2b85d0ef2d5129c4c0534c6241844
 
-router.get('/kirim', function(req, res){
 
-kirim();
-
-	res.send(kirim);
-});
-
-router.post('/upload', uploading.single, function (req, res) {
-console.log('ada akses upload');
-});
 
 
 module.exports = router;
