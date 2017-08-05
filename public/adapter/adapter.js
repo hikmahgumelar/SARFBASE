990dfbd7fd3b3bad2f26aba98f1e02be95518d7f
root <root@hydra-old.ibstower.com>
2017-08-05 07:49:51 +0700

    fix conflict

--- public/adapter/adapter.js
+++ public/adapter/adapter.js
@@ -1,12 +1,9 @@
 $(document).ready( function(){
 setInterval(function(){
-<<<<<<< HEAD
-   $( "#table" ).load( "/monitoring #table" );
-=======
    $( "#rubah" ).load( "/monitoring #rubah" );
    
 
->>>>>>> f1f4c89b91dfcb442c23047b06d7dec2da89bd74
+
 }, 700);
 });
 
